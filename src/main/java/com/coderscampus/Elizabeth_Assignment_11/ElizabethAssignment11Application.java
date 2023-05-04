3793491f682562ee5d19dc81c0453a4cdf901a97
Elizabeth <namelesselizabeth@gmail.com>
2023-05-04 12:31:23 -0700

    Initial commit

---
+++ src/main/java/com/coderscampus/Elizabeth_Assignment_11/ElizabethAssignment11Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Elizabeth_Assignment_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElizabethAssignment11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElizabethAssignment11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/Elizabeth_Assignment_11/ElizabethAssignment11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Elizabeth_Assignment_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElizabethAssignment11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
